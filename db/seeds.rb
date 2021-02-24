01f0daa5153ad104b167b3f3c908e6722294e1e9
Sanchodelniglo <rgrossard@gmail.com>
2021-02-24 23:46:48 +0100

    Add 5 questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Deleting models ...'
+
+Question.destroy_all
+
+puts 'Adding models ...'
+
+question = Question.create!(prompt: 'Que veulent dire les chiffres au fond des verres de cantine ?')
+question.answers.create!(
+  [
+    { assertion: 'C’est l’âge que vous avez et le plus jeune va chercher l’eau.' },
+    { assertion: 'C’est la note que vous aurez à l’interro de cet après-midi.' },
+    { assertion: 'C’est le nombre de filles avec qui vous sortirez dans la vie.' },
+    { assertion: 'On sait pas, ça reste un mystère mais on est tous sur le coup…', correct: true }
+  ]
+)
+
+question = Question.create!(prompt: "Si on avait persuadé Elvis Presley et Bob Marley de s'accoupler, on aurait pu obtenir :")
+question.answers.create!(
+  [
+    { assertion: 'Elvis Marley, le rasta king.' },
+    { assertion: 'Une baleine blanche avec des dreads.' },
+    { assertion: 'Des tubes comme « Spliff me tender » ou « Could you be cruel »' },
+    { assertion: 'Oui mais alors on dit n’importe quoi et toutes les réponses sont possibles.', correct: true }
+  ]
+)
+
+question = Question.create!(prompt: 'Selon la légende, pourquoi la ligne ferroviaire Moscou - St Petersbourg fait-elle un détour de 17km ?')
+question.answers.create!(
+  [
+    { assertion: 'Parce que les ouvriers étaient bourrés.' },
+    { assertion: 'Parce que le froid a tordu les rails.' },
+    { assertion: 'Parce que le Tsar avait laissé un doigt dépasser de sa règle.', correct: true },
+    { assertion: 'Parce qu’avant il y avait un village qui a disparu depuis.' }
+  ]
+)
+
+question = Question.create!(prompt: 'Quand la série "Derrick" s’est arrêtée, son scénariste avait :')
+question.answers.create!(
+  [
+    { assertion: '74 ans' },
+    { assertion: '84 ans' },
+    { assertion: '94 ans', correct: true },
+    { assertion: 'Honte' }
+  ]
+)
+
+question = Question.create!(prompt: 'Parmi ces 4 voitures, laquelle a vraiment existé ?')
+question.answers.create!(
+  [
+    { assertion: 'La Skoda « Tapina »' },
+    { assertion: 'La Nissan « Gigolo »' },
+    { assertion: 'La Mazda « Laputa »', correct: true },
+    { assertion: 'La Fiat « 500 l’amour et 200 la p*** »' }
+  ]
+)
